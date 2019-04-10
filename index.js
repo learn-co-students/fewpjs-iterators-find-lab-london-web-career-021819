84cedd4db200070d8dd6ddfc0b989ffecdcfa1ed
Alex Bujenita <dekc89@gmail.com>
2019-04-10 12:19:16 +0100

    done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const testVar = {}
+const testVar = {};
 
-function testFunc() {
-  return "hi"
+function superbowlWin(array) {
+   let year = array.find(obj => obj.result === "W");
+   if (year) {
+     return year.year;
+   } else {
+     return undefined;
+   }
 }
