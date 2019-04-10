0f1b0b104681cdffbb0b8b3bb5cc7ac89841ac2d
github username <github email address>
2019-04-10 11:31:50 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = (record) => {
+  let champWon = record.find(championship => championship.result === "W")
+  if (champWon) {
+    return champWon.year
+  } 
+}
