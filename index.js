0317cc36ab8d89c1a961cdd88903b5b909b98b69
schazbot <charlotte.pearce1984@gmail.com>
2019-09-16 13:50:34 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array) { 
+  const winner = array.find(object => object.result === "W")
+  return winner ? winner.year : winner
+}
